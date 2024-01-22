0b2b382528351d8a801bcd61bb3053f223d7565d
JustFiesta <mateusz.bocak9@gmail.com>
2024-01-22 15:01:31 +0100

    Add project files.

---
+++ FirstSteps/FirstSteps/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String chuj = "chuj";
+
+if (chuj is not String)
+{
+    Console.WriteLine("ty kurwo");
+}
+else
+{
+    Console.WriteLine("sztywniutko");
+}
+
+Console.WriteLine(chuj);
